61ab7d9cce6a7ab867182f5a5857ad28b879bf90
Nimesha511 <nimeshatharushi222@gmail.com>
2026-04-22 11:19:55 +0530

    updated
    updated

--- backend/src/main/java/com/smartcampus/repository/BookingRepository.java
+++ backend/src/main/java/com/smartcampus/repository/BookingRepository.java
@@ -5,7 +5,9 @@ import com.smartcampus.entity.Booking.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
