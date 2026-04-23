98da7bdc83f4f3bb37d8f91bd6b033cb43d7f195
ThevinduPesara <thevindupesara@gmail.com>
2026-04-23 15:36:35 +0530

    Create ResourceRepository.java

---
+++ backend/src/main/java/com/smartcampus/repository/ResourceRepository.java
@@ -0,0 +1,9 @@
+package com.smartcampus.repository;
+
+import com.smartcampus.entity.Resource;
+import com.smartcampus.entity.Resource.ResourceStatus;
+import com.smartcampus.entity.Resource.ResourceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
