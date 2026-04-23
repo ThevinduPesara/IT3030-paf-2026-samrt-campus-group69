d9c98f66b77896dc4bb3843fdcdb3e7009398d8b
ThevinduPesara <thevindupesara@gmail.com>
2026-04-23 15:42:28 +0530

    Create ResourceController.java

---
+++ backend/src/main/java/com/smartcampus/controller/ResourceController.java
@@ -0,0 +1,13 @@
+package com.smartcampus.controller;
+
+import com.smartcampus.entity.Resource;
+import com.smartcampus.entity.Resource.ResourceStatus;
+import com.smartcampus.entity.Resource.ResourceType;
+import com.smartcampus.service.ResourceService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
